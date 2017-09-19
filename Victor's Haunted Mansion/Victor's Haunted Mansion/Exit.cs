eccce8e8a3038d6505f2f88fff032fb63b75cf5c
Victor Hermansson <hermansson_79@hotmail.com>
2017-09-19 11:46:46 +0200

    Exit class made

--- Victor's Haunted Mansion/Victor's Haunted Mansion/Exit.cs
+++ Victor's Haunted Mansion/Victor's Haunted Mansion/Exit.cs
@@ -8,5 +8,34 @@ namespace Victor_s_Haunted_Mansion
 {
     class Exit
     {
+        public string Name { get; private set; }
+        private bool isLocked;
+        private string informationLocked;
+        private string informationOpen;
+        private string unlockItem;
+        
+        public Exit(string name, bool locked, string informationLocked, string informationOpen, string unlockItem)
+        {
+            Name = name;
+            isLocked = locked;
+            this.informationLocked = informationLocked;
+            this.informationOpen = informationOpen;
+            this.unlockItem = unlockItem;        
+        }
+        public void UseItemOn(Item item)
+        {
+            if (unlockItem == item.Name)
+            {
+                isLocked = true;
+            }
+        }
+        public string GetInformation()
+        {
+            if (isLocked)
+            {
+                return informationLocked;
+            }
+            return informationOpen;
+        }
     }
 }
