5c9d02617cb4351ef63e780ee9e95431d60afccd
Gustav Westberg <gustavwes@gmail.com>
2017-09-19 10:29:25 +0200

    Test för reository

---
+++ Victor's Haunted Mansion/Victor's Haunted Mansion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victor_s_Haunted_Mansion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej test");
+        }
+    }
+}
