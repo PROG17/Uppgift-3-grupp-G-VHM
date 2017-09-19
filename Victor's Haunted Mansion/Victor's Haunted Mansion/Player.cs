7668dcfe55471fcb2229368422381722f9719a3f
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2017-09-19 11:23:22 +0200

    La till klasser, fixat Item.cs

---
+++ Victor's Haunted Mansion/Victor's Haunted Mansion/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victor_s_Haunted_Mansion
+{
+    class Player
+    {
+    }
+}
