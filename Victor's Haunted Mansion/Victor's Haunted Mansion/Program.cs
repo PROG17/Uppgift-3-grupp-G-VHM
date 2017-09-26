d75ee3d4a03e86f242f5be06abe3fbfe5e3c44a8
Gustav Westberg <gustavwes@gmail.com>
2017-09-26 11:17:03 +0200

    Added play again functionality

--- Victor's Haunted Mansion/Victor's Haunted Mansion/Program.cs
+++ Victor's Haunted Mansion/Victor's Haunted Mansion/Program.cs
@@ -11,8 +11,30 @@ namespace Victor_s_Haunted_Mansion
         static void Main(string[] args)
         {
             Console.Title = "Victors Haunted Mansion";
-            Game game = new Game();
-            game.PlayGame();
+            bool playing = true;
+            bool validAnswer = true;
+
+            do
+            {
+            Console.Clear();
+                Game game = new Game();
+                game.PlayGame();
+                while (validAnswer)
+                {
+                    Console.WriteLine("Would you like to try again? (Yes/No)");
+
+                    string yesNo = Console.ReadLine();
+
+                    if (yesNo.ToLower() == "yes")
+                        validAnswer = false;
+                    if (yesNo.ToLower() == "no")
+                    {
+                        playing = false;
+                        break;
+                    }
+
+                }
+            } while (playing);
         }
     }
 }
